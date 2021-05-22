d6393b6ed69810eb2d7c712d98eb560307932f16
andrewhuang192 <andrewhuang192@gmail.com>
2021-05-21 17:33:16 -0700

    models update

--- flutter/main_app/admin.py
+++ flutter/main_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Photo, Comment, Like
+
+admin.site.register(User)
+admin.site.register(Photo)
+admin.site.register(Comment)
+admin.site.register(Like)
