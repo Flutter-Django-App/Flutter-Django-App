deb2a37123271a0373a11ab9aa4c1aacdd8848fd
Edith Rodriguez <edithrodriguez@Ediths-MacBook-Air.local>
2021-05-23 12:45:47 -0700

    Add login/signup functionality to React frontend side

--- flutter/main_app/urls.py
+++ flutter/main_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from .views import current_user, UserList
 
 urlpatterns = [
   path('', views.home, name='home'),
   path('photos/', views.photos_index, name='index'),
   path('accounts/signup/', views.signup, name='signup'),
-]
+  path('current_user/', current_user),
+  path('users/', UserList.as_view())
+] 
