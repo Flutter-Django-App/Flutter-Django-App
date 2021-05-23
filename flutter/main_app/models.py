247b6d2779e6157a7654e79c67e8f2270a425b09
Kevin Decker <KevinDecker64@gmail.com>
2021-05-23 18:28:59 -0500

    refactors models

--- flutter/main_app/models.py
+++ flutter/main_app/models.py
@@ -3,62 +3,36 @@ from datetime import date
 from django.contrib.auth.models import User
 
 
-# # Create your models here.
-# class User(models.Model):
-#     first_name = models.CharField(max_length=25)
-#     last_name = models.CharField(max_length=25)
-#     email = models.CharField(
-#         max_length=25, 
-#         blank=False
-#     )
-#     password = models.CharField(
-#         max_length=25,
-#         # default=8,
-#         blank=False
-#     )
-#     username = models.CharField(
-#         max_length=25,
-#         unique=True,
-#         blank=False
-#     )
-#     created_date = models.DateField('user created date')
-
-#     def __str__(self):
-#         return self.username
-
 class Photo(models.Model):
-    total_likes = models.IntegerField()
-    total_comments = models.IntegerField()
-    caption = models.CharField(max_length=140)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="photos")
+    caption = models.CharField(max_length=500)
     location = models.CharField(max_length=100)
     #below url is for storing the photo resource from AWS
     url = models.CharField(max_length=200)
-    created_date = models.DateField('photo created date')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
     # tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.caption
 
-    # def __str__(self):
-    #     return f"Photo for plant_id: {self.plant_id} @{self.url}"
-
 class Comment(models.Model):
-    total_likes = models.IntegerField()
-    comment = models.TextField(
-        max_length=140,
-        blank=False
-    )
-    created_date = models.DateField('comment created date')
-    photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE, related_name="comments")
+    comment = models.TextField(max_length=140, blank=False)
+    created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.comment
+        return f"Comment on Photo-{self.photo_id} by User-{self.user_id}: {self.comment}"
 
 class Like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE, related_name="likes")
+
+    def __str__(self):
+        return f"Photo-{self.photo_id} liked by User-{self.user_id}"
 
 # class Tag(models.Model):
-#     name = models.CharField(max_length=15)
+#     tag = models.CharField(max_length=85)
+
+#     def __str__(self):
+#         return f"Tag: {self.tag}"
